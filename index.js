d40f3aff12403cc527b8d88c22033739f8c7d078
Pedro Lacerda <pedrolacerda@github.com>
2019-12-12 22:39:27 -0300

    Fix parser and .gitignore

--- index.js
+++ index.js
@@ -4,29 +4,63 @@
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
- */
+ */https://github.com/pedrolacerda/maximo-gmud.git
+
 module.exports = app => {
   app.on(['check_suite.requested', 'check_run.rerequested'], check)
 
   async function check (context) {
+    var fs = require('fs')
+    var path = require('path')
+    var filePath = path.join(__dirname, 'maximo.config');
+    var stringMaximo = fs.readFileSync(filePath,{ encoding: 'utf8' })
+    
+    var maximoList = stringMaximo.split('|')
+    var maximoJson = {}
+    maximoList.forEach(element => {
+      var thisElementSplited = element.split('=')
+      maximoJson[thisElementSplited[0]]= thisElementSplited[1]
+    });
+
+    var maximoStatus = maximoJson['STATUS']
+
+    console.log("É 'Aprovado'? " + maximoStatus.localeCompare('APROVADO'))
+
     const startTime = new Date()
 
     // Do stuff
     const { head_branch: headBranch, head_sha: headSha } = context.payload.check_suite
     // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
-    return context.github.checks.create(context.repo({
-      name: 'My app!',
-      head_branch: headBranch,
-      head_sha: headSha,
-      status: 'completed',
-      started_at: startTime,
-      conclusion: 'success',
-      completed_at: new Date(),
-      output: {
-        title: 'Probot check!',
-        summary: 'The check has passed!'
-      }
-    }))
+    if(maximoStatus.localeCompare('APROVADO') == 0){
+      return context.github.checks.create(context.repo({
+        name: 'Valida Item GMUD Máximo',
+        head_branch: headBranch,
+        head_sha: headSha,
+        status: 'completed',
+        started_at: startTime,
+        conclusion: 'success',
+        completed_at: new Date(),
+        output: {
+          title: 'Validação Máximo',
+          summary: 'O Item de GMUD foi aprovado!'
+        }
+      }))
+    } else {
+      return context.github.checks.create(context.repo({
+        name: 'Valida Item GMUD Máximo',
+        head_branch: headBranch,
+        head_sha: headSha,
+        status: 'completed',
+        started_at: startTime,
+        conclusion: 'failure',
+        completed_at: new Date(),
+        output: {
+          title: 'Validação Máximo',
+          summary: 'O Item de GMUD <b>NÃO</b> está aprovado!'
+        }
+      }))
+    }
+    
   }
 
   // For more information on building apps:
